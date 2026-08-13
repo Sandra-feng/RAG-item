53c85dac8cdc2cf747eb0b65e67d9a08461fb68a
Sandra-feng <53212871+Sandra-feng@users.noreply.github.com>
2026-08-13 10:21:29 +0800

    Add markdown loader and vector store integration

--- RAG-simple.py
+++ RAG-simple.py
@@ -1,2 +1,61 @@
 import os
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import UnstructuredMarkdownLoader
+from langchain_text_splitters import MarkdownTextSplitter,RecursiveCharacterTextSplitter
+from langchain_huggingface_hub import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain_core.vectorstores import VectorStore
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+markdown_path = "../../data/C1/markdown/easy-rl-chapter1.md"
+
+# 加载本地markdown文件
+loader = UnstructuredMarkdownLoader(markdown_path)
+docs = loader.load()
+
+
+# 将文档拆分为更小的块
+# text_splitter = MarkdownTextSplitter(chunk_size=1000, chunk_overlap=0)
+text_splitter = RecursiveCharacterTextSplitter()
+chunks = text_splitter.split_documents(docs)
+
+# 中文嵌入模型
+embeddings = HuggingFaceEmbeddings(
+    model_name="BAAI/bge-small-zh-v1.5",
+    model_kwargs={'device': 'cpu'},
+    encode_kwargs={'normalize_embeddings': True}
+)
+# 使用HuggingFaceEmbeddings将文本块转换为向量
+# embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+# 创建FAISS向量存储 
+vectorstore = FAISS.from_documents(chunks, embeddings)
+
+# 提示词模板
+prompt = ChatPromptTemplate.from_template("""请根据下面提供的上下文信息来回答问题。
+请确保你的回答完全基于这些上下文。
+如果上下文中没有足够的信息来回答问题，请直接告知：“抱歉，我无法根据提供的上下文找到相关信息来回答此问题。”
+
+上下文:
+{context}
+
+问题: {question}
+
+回答:""")
+
+llm = ChatOpenAI(
+    model="deepseek-chat",
+    temperature=0.7,
+    max_tokens=4096,
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+    base_url="https://api.deepseek.com"
+)
+
+# 用户查询
+question = "文中举了哪些例子？"
+
+# 在向量存储中查询相关文档
+retrieved_docs = vectorstore.similarity_search(question, k=3)
+docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)
+
+answer = llm.invoke(prompt.format(question=question, context=docs_content))
+print(answer)
